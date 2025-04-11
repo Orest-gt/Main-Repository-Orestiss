2aa609468528810e4fca976fbd76c051b243fe10
Orestis <orestisgatos@gmail.com>
2025-04-11 16:54:18 +0300

    Dataset

---
+++ MLP/dataset.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+X = np.random.rand(10, 5)
+y = np.array([
+    [1, 0],
+    [0, 1],
+    [1, 0],
+    [0, 1],
+    [1, 0],
+    [0, 1],
+    [1, 0],
+    [0, 1],
+    [1, 0],
+    [0, 1],
+])
+
+'''
+Clarification:
+
+You can import your own dataset, but you make sure to change the needed parameter
+And change the loss functions (i will do it to make it automatic in the future)
+
+This is just a small and quick dataset to test my MLP model!
+
+'''
